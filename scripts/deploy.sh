4c56dcdbf4974d7ba503144ce1eb38737546910c
qader1987 <qader.mohammed22@gmail.com>
2020-07-12 15:41:55 -0400

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
-#This script can be used to deploy the stack with 'onica_test.yaml'
+#This script can be used to create/update the stack with 'onica_test.yaml'
 
-aws cloudformation create-stack --stack-name test-application --template-body ../templates/onica_test.yaml \
-    --region us-east-1  --profile ${profile} \
-    --parameters  "ParameterKey=Env,ParameterValue=test"
+echo "Deploying CloudFormation stack"
+aws cloudformation deploy --stack-name test-application --template-file ../templates/onica_test.yaml \
+    --region us-east-1 --parameter-overrides  "ParameterKey=Env,ParameterValue=test"
