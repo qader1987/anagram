8bc6326369e93ccf6ddb6bc97b1aa737fa952e1b
qader1987 <qader.mohammed22@gmail.com>
2020-07-30 22:40:21 -0400

    updated autoscaling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
-#This script can be used to create/update the stack with 'onica_test.yaml'
+#This script can be used to create/update the stack with 'infra.yaml'
 
 echo "Deploying CloudFormation stack"
-aws cloudformation deploy --stack-name test-application --template-file ../templates/infra.yaml \
-    --region us-east-1 --parameter-overrides  "ParameterKey=Env,ParameterValue=test"
+aws cloudformation deploy --stack-name anagram-test --template-file ../templates/infra.yaml \
+    --region us-east-1 --parameter-overrides  Env=test KeyName=qader
