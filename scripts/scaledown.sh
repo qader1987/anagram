358249efe8a2c1ef7c89d38e153140984431c79d
qader1987 <68161948+qader1987@users.noreply.github.com>
2020-08-04 23:52:27 -0400

    Update app (#13)
    * deleted old file
    
    * update userdata
    
    * removed file
    
    * cleanup
    
    * fixed name

--- scripts/scaledown.sh
+++ scripts/scaledown.sh
@@ -2,4 +2,4 @@
 #This command will trigger scale down policy
 
 echo "Executing scale down policy"
-aws autoscaling execute-policy --auto-scaling-group-name "test-application-AutoScalingGroup-CBLTI2S6AYOH" --policy-name "test-application-scaleDownPolicy-1079VHSABRCWB" --no-honor-cooldown
+aws autoscaling execute-policy --auto-scaling-group-name "anagram-test-AutoScalingGroup-UHJCKK3PYKYS" --policy-name "anagram-test-scaleDownPolicy-1XXPB37COVMRA" --no-honor-cooldown
--- scripts/scaleup.sh
+++ scripts/scaleup.sh
@@ -2,4 +2,4 @@
 #This command will trigger scale up policy
 
 echo "Executing scale up policy"
-aws autoscaling execute-policy --auto-scaling-group-name "test-application-AutoScalingGroup-CBLTI2S6AYOH" --policy-name "test-application-scaleUpPolicy-1DNDLJZ0EGBTR" --no-honor-cooldown
+aws autoscaling execute-policy --auto-scaling-group-name "anagram-test-AutoScalingGroup-UHJCKK3PYKYS" --policy-name "anagram-test-scaleUpPolicy-1SNZFQQYIDFAN" --no-honor-cooldown
