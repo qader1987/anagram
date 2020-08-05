e6678dd4ed80948233cc740dd02cb08870312af7
Qader Mohammed <qader.mohammed22@gmail.com>
2020-08-04 23:51:38 -0400

    fixed name

--- scripts/scaledown.sh
+++ scripts/scaledown.sh
@@ -2,4 +2,4 @@
 #This command will trigger scale down policy
 
 echo "Executing scale down policy"
-aws autoscaling execute-policy --auto-scaling-group-name "anagram-test-scaleDownPolicy-1XXPB37COVMRA" --no-honor-cooldown
+aws autoscaling execute-policy --auto-scaling-group-name "anagram-test-AutoScalingGroup-UHJCKK3PYKYS" --policy-name "anagram-test-scaleDownPolicy-1XXPB37COVMRA" --no-honor-cooldown
--- scripts/scaleup.sh
+++ scripts/scaleup.sh
@@ -2,4 +2,4 @@
 #This command will trigger scale up policy
 
 echo "Executing scale up policy"
-aws autoscaling execute-policy --auto-scaling-group-name "anagram-test-scaleDownPolicy-1XXPB37COVMRA" --policy-name "anagram-test-scaleUpPolicy-1SNZFQQYIDFAN" --no-honor-cooldown
+aws autoscaling execute-policy --auto-scaling-group-name "anagram-test-AutoScalingGroup-UHJCKK3PYKYS" --policy-name "anagram-test-scaleUpPolicy-1SNZFQQYIDFAN" --no-honor-cooldown
